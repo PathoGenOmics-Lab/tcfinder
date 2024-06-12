266f905ecc46190c1bd2ec832634feb5a8fc731d
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-06-12 10:18:00 +0200

    Add target annotation

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ struct NodeW {
     is_target: bool,
 }
 
+/// Read a phylogeny in phylo4 format
 fn read_phylo4<T: std::io::Read>(reader: T) -> Result<Graph<NodeW, ()>, Box<dyn Error>> {
     // Init tree
     let mut tree: Graph<NodeW, ()> = Graph::new();
@@ -59,8 +60,12 @@ fn read_phylo4<T: std::io::Read>(reader: T) -> Result<Graph<NodeW, ()>, Box<dyn 
     Ok(tree)
 }
 
-fn annotate_targets(tree: &mut Graph<NodeW, ()>, targets: &Vec<String>) -> Graph<NodeW, ()> {
-    todo!();
+/// Annotates targets in place
+fn annotate_targets(tree: &mut Graph<NodeW, ()>, targets: &Vec<String>) {
+    for node in tree.node_indices() {
+        let weight = tree.node_weight_mut(node).unwrap();
+        weight.is_target = targets.contains(&weight.label);
+    }
 }
 
 fn main() -> io::Result<()> {
@@ -72,15 +77,51 @@ fn main() -> io::Result<()> {
 mod tests {
     use super::*;
 
+    fn read_test_tree() -> Graph<NodeW, ()> {
+        let file = File::open("test/rtree.csv").expect("Could not open tree file");
+        read_phylo4(file).expect("Cannot parse tree")
+    }
+
+    fn read_test_targets() -> Vec<String> {
+        let file = File::open("test/targets.txt").expect("Could not open targets file");
+        let buf = BufReader::new(file);
+        buf.lines()
+            .map(|line| line.expect("Could not parse line"))
+            .collect()
+    }
+
     #[test]
     fn read_tree() {
-        let file = File::open("test/rtree.csv").expect("Cannot open tree file");
-        let tree = read_phylo4(file).expect("Cannot parse tree");
+        let tree = read_test_tree();
+    }
+
+    #[test]
+    fn count_tips() {
+        let tree = read_test_tree();
         let n_tips = tree
             .node_indices()
             .map(|node| tree.node_weight(node))
             .filter(|w| w.unwrap().is_tip)
             .count();
         assert_eq!(n_tips, 100);
+    }
+
+    #[test]
+    fn count_targets() {
+        let targets = read_test_targets();
+        assert_eq!(targets.len(), 6);
+    }
+
+    #[test]
+    fn count_target_tips() {
+        let mut tree = read_test_tree();
+        let targets = read_test_targets();
+        annotate_targets(&mut tree, &targets);
+        let n_targets = tree
+            .node_indices()
+            .map(|node| tree.node_weight(node))
+            .filter(|w| w.unwrap().is_target)
+            .count();
+        assert_eq!(n_targets, 6);
     }
 }
