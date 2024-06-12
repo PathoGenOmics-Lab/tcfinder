4779973bda34d19f813e0b4860e9d338ee3cbeb2
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-06-12 09:53:05 +0200

    Complete function to read phylo4 tree

--- src/main.rs
+++ src/main.rs
@@ -4,66 +4,66 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 use csv;
-use petgraph::graph::NodeIndex;
-use petgraph::Graph;
-
+use petgraph::prelude::*;
 
 #[derive(serde::Deserialize, Debug)]
 struct Phylo4Row {
     label: String,
     node: usize,
     ancestor: usize,
-    nodetype: String
+    nodetype: String,
 }
-
 
 struct NodeW {
     index: usize,
     label: String,
     is_tip: bool,
-    is_target: bool
+    is_target: bool,
 }
 
-
-struct EdgeW {
-    distance: f64
-}
-
-
-fn read_phylo4<T: std::io::Read>(reader: T) -> Result<Graph<NodeW, EdgeW>, Box<dyn Error>> {
+fn read_phylo4<T: std::io::Read>(reader: T) -> Result<Graph<NodeW, ()>, Box<dyn Error>> {
     // Init tree
-    let mut tree: Graph<NodeW, EdgeW> = Graph::new();
+    let mut tree: Graph<NodeW, ()> = Graph::new();
     let mut tree_index: HashMap<usize, NodeIndex> = HashMap::new();
     // Init reader
     let mut rdr = csv::Reader::from_reader(reader);
     let headers = rdr.headers()?.clone();
+    // First pass: insert nodes
     for result in rdr.records() {
         // Parse row
         let record = result?;
         let row: Phylo4Row = record.deserialize(Some(&headers))?;
         // Insert node in tree
         let is_tip: bool = row.nodetype == "tip";
-        let weight = NodeW { index: row.node, label: row.label, is_tip, is_target: false };
+        let weight = NodeW {
+            index: row.node,
+            label: row.label,
+            is_tip,
+            is_target: false,
+        };
         let node_index = tree.add_node(weight);
         // Insert node in index
         tree_index.insert(row.node, node_index);
     }
-    // TODO: add edges
-    todo!();
+    // Second pass: insert edges
+    for result in rdr.records() {
+        // Parse row
+        let record = result?;
+        let row: Phylo4Row = record.deserialize(Some(&headers))?;
+        // Get source (ancestor) and target (node) from index
+        let &source = tree_index.get(&row.ancestor).unwrap();
+        let &target = tree_index.get(&row.node).unwrap();
+        // Insert edge in tree
+        tree.add_edge(source, target, ());
+    }
     Ok(tree)
 }
 
-
-fn annotate_targets(tree: &mut Graph<NodeW, EdgeW>, targets: &Vec<String>) -> Graph<NodeW, EdgeW> {
+fn annotate_targets(tree: &mut Graph<NodeW, ()>, targets: &Vec<String>) -> Graph<NodeW, ()> {
     todo!();
 }
 
-
 fn main() -> io::Result<()> {
-    let file = File::open("tree.csv")?;
-    
-    let tree = read_phylo4(file);
-    // println!("{:?}", tree);
-
+    todo!();
     Ok(())
 }
